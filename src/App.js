94989c9c7791145cc3d3c454af8aed785335f840
ronbp <ronmagpantay9513@gmail.com>
2022-02-21 17:14:09 -0800

    still working on container placement but will need to rearrange some divs

--- src/App.js
+++ src/App.js
@@ -8,12 +8,35 @@ function App() {
 
         <div class="leftCont">
           <div id="leftMenu">
-
+            <h1>Side Menu</h1>
           </div>
         </div>
         
         <div class="rightCont">
-          <p>rest of content</p>
+          
+          <div class="leftSide">
+
+            <div class="leftButtons">
+              <p class="button">Buhay</p>
+              <p class="button">Streak</p>
+            </div>
+            <div class="widgetMenu">
+              <p>widget1</p>
+              <p>widget2</p>
+              <p>widget3</p>
+              <p>widget4</p>
+              <p>widget5</p>
+              <p>widget6</p>
+            </div>
+
+          </div>
+
+          <div class="rightSide">
+            <p class="button">Signin</p>
+            <p class="button">FS B</p>
+            <p class="button">hamMenu</p>
+          </div>
+
         </div>
       
       </div>
