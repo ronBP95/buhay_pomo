e0e685bc57feec6d5a2bf50c8579d12d43132cae
ronbp <ronmagpantay9513@gmail.com>
2022-02-22 17:11:57 -0800

    overhauled the previous containers setup and its looking much better. need to work on making certain elements non-responsive to window re-sizing

--- src/App.js
+++ src/App.js
@@ -2,42 +2,41 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div class="page">
+      
+      <div class="leftPage">
+        <div class="mainMenu">
+          <p> SIDE MENU</p>
+        </div>
+      </div>
+      <div class="rightPage">
 
-      <div class="bigCont">
-
-        <div class="leftCont">
-          <div id="leftMenu">
-            <h1>Side Menu</h1>
+        <div class="topButtons">
+          <div class="logoButtons">
+            <p class="button">LOGO</p>
+            <p class="button">LOGO</p>
           </div>
+          
+          <div class="signButtons">
+            <p class="button">Signin</p>
+            <p class="button">FS</p>
+            <p class="button">MENU</p>
+          </div>
+
+        </div>
+        <div class="widgets">
+          <p>TOOLS</p>
+          <p>W</p>
+          <p>W</p>
+          <p>W</p>
+          <p>W</p>
+          <p>W</p>
+          <p>W</p>
+          <p>+</p>
+          <div><hr></hr></div>
+          <p>Reset</p>
         </div>
         
-        <div class="rightCont">
-
-            <div class="allButtons">  
-              <div class="leftSide">
-                <p class="button">Buhay</p>
-                <p class="button">Streak</p>
-              </div>
-
-              <div class="rightSide">
-                <p class="button">Signin</p>
-                <p class="button">FS B</p>
-                <p class="button">hamMenu</p>
-              </div>
-            </div>
-
-            <div class="widgetMenu">
-              <p>widget1</p>
-              <p>widget2</p>
-              <p>widget3</p>
-              <p>widget4</p>
-              <p>widget5</p>
-              <p>widget6</p>
-            </div>
-
-        </div>
-      
       </div>
 
     </div>
