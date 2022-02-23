d61b9cfb03957658a2744d88994904e7ae91e8e8
ronbp <ronmagpantay9513@gmail.com>
2022-02-23 01:20:46 -0800

    significant progress with widget bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
 
+// React Icon Imports
+import { CgArrowsExpandRight,  } from "react-icons/cg";
+import { BsFillPersonFill, BsFillVolumeDownFill } from "react-icons/bs";
+import { BiDotsVerticalRounded } from "react-icons/bi";
+import { RiImageLine, RiTimerLine, RiSoundModuleFill, RiTodoLine } from "react-icons/ri";
+import { GiCrystalBall } from "react-icons/gi";
+
 function App() {
   return (
     <div class="page">
@@ -13,25 +20,43 @@ function App() {
 
         <div class="topButtons">
           <div class="logoButtons">
-            <p class="button">LOGO</p>
-            <p class="button">LOGO</p>
+            <p class="button">Buhay</p>
+            <p class="button">3 🔥</p>
           </div>
           
           <div class="signButtons">
-            <p class="button">Signin</p>
-            <p class="button">FS</p>
-            <p class="button">MENU</p>
+            <p class="button">🚀 Sign Up</p>
+            <p class="button"><CgArrowsExpandRight /></p>
+            <p class="button"><BsFillPersonFill /><BiDotsVerticalRounded /></p>
           </div>
 
         </div>
         <div class="widgets">
-          <p>TOOLS</p>
-          <p>W</p>
-          <p>W</p>
-          <p>W</p>
-          <p>W</p>
-          <p>W</p>
-          <p>W</p>
+            <p>TOOLS</p>
+          <div class="widgetCard">
+            <div class="icon"><RiImageLine size={25}/></div>
+            <p>Spaces</p>
+          </div>
+          <div class="widgetCard">
+            <div class="icon"><RiTimerLine size={25}/> </div>
+            <p>Timer</p>
+          </div>
+          <div class="widgetCard">
+          <div class="icon"><BsFillVolumeDownFill size={25} /> </div>    
+            <p>Music</p>
+          </div>
+          <div class="widgetCard">
+          <div class="icon"><RiSoundModuleFill size={25} /> </div> 
+            <p>Sounds</p>
+          </div>
+          <div class="widgetCard">
+          <div class="icon"><RiTodoLine size={25} /> </div> 
+          <p>To-Do</p>
+          </div>
+          <div class="widgetCard">
+          <div class="icon"><GiCrystalBall size={25} /> </div> 
+            <p>Fortune</p>
+          </div>
           <p>+</p>
           <div><hr></hr></div>
           <p>Reset</p>
