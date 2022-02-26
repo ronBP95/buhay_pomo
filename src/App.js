577dba5f6779a7f8933bacc6573c7f8669b98178
ronbp <ronmagpantay9513@gmail.com>
2022-02-26 15:42:09 -0800

    Widgets menu was migrated back to the app.js for the purpose of managing show/hide components

--- src/App.js
+++ src/App.js
@@ -9,14 +9,21 @@ import { BiDotsVerticalRounded, BiVolumeMute } from "react-icons/bi";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { AiOutlineHeart, AiFillInstagram, AiFillYoutube } from "react-icons/ai";
 import { ImMenu3 } from "react-icons/im"
+import { GiCrystalBall } from "react-icons/gi";
+import { RiImageLine, RiTimerLine, RiSoundModuleFill, RiTodoLine } from "react-icons/ri";
 
 // Component Imports
 import Timer from "../src/components/Timer"
-import Widgets from "../src/components/Widgets"
 
 function App() {
   
   const handle = useFullScreenHandle();
+
+  // Reset Function for Widgets Menu
+  const Reset = () => {
+    console.log("clicked")
+    window.location.reload();
+  }
 
   return (
     <FullScreen handle={handle}>
@@ -125,7 +132,36 @@ function App() {
         </div>
 
         {/* Widgets Menu */}
-        <Widgets />
+        <div class="widgets">
+            <p id="widgetHeader">TOOLS</p>
+          <div class="spacesCard">
+            <div class="icon"><RiImageLine size={25}/></div>
+            <p>Spaces</p>
+          </div>
+          <div class="timerCard">
+            <div class="icon"><RiTimerLine size={25}/> </div>
+            <p>Timer</p>
+          </div>
+          <div class="musicCard">
+          <div class="icon"><BsFillVolumeDownFill size={25} /> </div>    
+            <p>Music</p>
+          </div>
+          <div class="soundsCard">
+          <div class="icon"><RiSoundModuleFill size={25} /> </div> 
+            <p>Sounds</p>
+          </div>
+          <div class="todoCard">
+          <div class="icon"><RiTodoLine size={25} /> </div> 
+          <p>To-Do</p>
+          </div>
+          <div class="fortuneCard">
+          <div class="icon"><GiCrystalBall size={25} /> </div> 
+            <p>Fortune</p>
+            <p id="widgetAdd">+</p>
+          </div>
+          <div><hr></hr></div>
+          <p id="widgetReset" onClick={Reset}>Reset</p>
+        </div>
         
         {/* Timer Widget */}
         <Draggable>
