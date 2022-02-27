1e6209b8a5506038d9c535256b4e47990c85d371
ronbp <ronmagpantay9513@gmail.com>
2022-02-26 19:45:44 -0800

    added toggles for widgets menu and added inputs. Will connect input to the state of the individual timers like in LifeAt

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,4 +1,4 @@
-import React,  { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import "../components/css/Timer.css"
 import { BsArrowClockwise, BsGearFill, BsDash } from "react-icons/bs";
 
@@ -6,19 +6,19 @@ const Alert = () => {
     alert("I'm sorry but this element is currently under construction!")
 }
 
-const Pomo = (props:any) => {
-    const {pomoMinutes = 45, pomoSeconds = 0} = props;
-    const [ minutes, setMinutes ] = useState(pomoMinutes);
-    const [ seconds, setSeconds ] = useState(pomoSeconds);
-    const [ isActive, setisActive] = useState(false)
+const Pomo = (props: any) => {
+    const { pomoMinutes = 45, pomoSeconds = 0 } = props;
+    const [minutes, setMinutes] = useState(pomoMinutes);
+    const [seconds, setSeconds] = useState(pomoSeconds);
+    const [isActive, setisActive] = useState(false)
 
-    function toggleOn () {
+    function toggleOn() {
         setisActive(!isActive)
     }
 
     useEffect(() => {
         if (isActive) {
-            let myInterval = setInterval (() => {
+            let myInterval = setInterval(() => {
                 if (seconds > 0) {
                     setSeconds(seconds - 1);
                 }
@@ -33,38 +33,38 @@ const Pomo = (props:any) => {
             }, 1000)
             return () => {
                 clearInterval(myInterval)
-            };  
+            };
         }
     });
     return (
         <div class="timerWidget">
-        <div class="clock">
-        { minutes === 0 && seconds === 0
-            ? null
-            : <h1> {minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
-        }
-        </div>
-        <div class="clockButtons">
-            <p class="startClock" onClick={toggleOn}>{isActive ? 'Pause' : 'Start'}</p>
-            <div><BsArrowClockwise size={25}/></div>
-        </div>
+            <div class="clock">
+                {minutes === 0 && seconds === 0
+                    ? null
+                    : <h1> {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+                }
+            </div>
+            <div class="clockButtons">
+                <p class="startClock" onClick={toggleOn}>{isActive ? 'Pause' : 'Start'}</p>
+                <div><BsArrowClockwise size={25} /></div>
+            </div>
         </div>
     )
 }
 
-const ShortBreak = (props:any) => {
-    const {pomoMinutes = 5, pomoSeconds = 0} = props;
-    const [ minutes, setMinutes ] = useState(pomoMinutes);
-    const [ seconds, setSeconds ] = useState(pomoSeconds);
-    const [ isActive, setisActive] = useState(false)
+const ShortBreak = (props: any) => {
+    const { pomoMinutes = 5, pomoSeconds = 0 } = props;
+    const [minutes, setMinutes] = useState(pomoMinutes);
+    const [seconds, setSeconds] = useState(pomoSeconds);
+    const [isActive, setisActive] = useState(false)
 
-    function toggleOn () {
+    function toggleOn() {
         setisActive(!isActive)
     }
 
     useEffect(() => {
         if (isActive) {
-            let myInterval = setInterval (() => {
+            let myInterval = setInterval(() => {
                 if (seconds > 0) {
                     setSeconds(seconds - 1);
                 }
@@ -79,38 +79,38 @@ const ShortBreak = (props:any) => {
             }, 1000)
             return () => {
                 clearInterval(myInterval)
-            };  
+            };
         }
     });
     return (
         <div class="timerWidget">
-        <div class="clock">
-        { minutes === 0 && seconds === 0
-            ? null
-            : <h1> {minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
-        }
-        </div>
-        <div class="clockButtons">
-            <p class="startClock" onClick={toggleOn}>{isActive ? 'Pause' : 'Start'}</p>
-            <div><BsArrowClockwise size={25}/></div>
-        </div>
+            <div class="clock">
+                {minutes === 0 && seconds === 0
+                    ? null
+                    : <h1> {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+                }
+            </div>
+            <div class="clockButtons">
+                <p class="startClock" onClick={toggleOn}>{isActive ? 'Pause' : 'Start'}</p>
+                <div><BsArrowClockwise size={25} /></div>
+            </div>
         </div>
     )
 }
 
-const LongBreak = (props:any) => {
-    const {pomoMinutes = 15, pomoSeconds = 0} = props;
-    const [ minutes, setMinutes ] = useState(pomoMinutes);
-    const [ seconds, setSeconds ] = useState(pomoSeconds);
-    const [ isActive, setisActive] = useState(false)
+const LongBreak = (props: any) => {
+    const { pomoMinutes = 15, pomoSeconds = 0 } = props;
+    const [minutes, setMinutes] = useState(pomoMinutes);
+    const [seconds, setSeconds] = useState(pomoSeconds);
+    const [isActive, setisActive] = useState(false)
 
-    function toggleOn () {
+    function toggleOn() {
         setisActive(!isActive)
     }
 
     useEffect(() => {
         if (isActive) {
-            let myInterval = setInterval (() => {
+            let myInterval = setInterval(() => {
                 if (seconds > 0) {
                     setSeconds(seconds - 1);
                 }
@@ -125,50 +125,92 @@ const LongBreak = (props:any) => {
             }, 1000)
             return () => {
                 clearInterval(myInterval)
-            };  
+            };
         }
     });
     return (
         <div class="timerWidget">
-        <div class="clock">
-        { minutes === 0 && seconds === 0
-            ? null
-            : <h1> {minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
-        }
-        </div>
-        <div class="clockButtons">
-            <p class="startClock" onClick={toggleOn}>{isActive ? 'Pause' : 'Start'}</p>
-            <div><BsArrowClockwise size={25}/></div>
-        </div>
+            <div class="clock">
+                {minutes === 0 && seconds === 0
+                    ? null
+                    : <h1> {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+                }
+            </div>
+            <div class="clockButtons">
+                <p class="startClock" onClick={toggleOn}>{isActive ? 'Pause' : 'Start'}</p>
+                <div><BsArrowClockwise size={25} /></div>
+            </div>
         </div>
     )
 }
 
 const Timer = () => {
-    const [ pomo, setPomo] = useState(false)
-    const [ short, setShort] = useState(false)
-    const [ long, setLong] = useState(false)
+    const [pomo, setPomo] = useState(true)
+    const [short, setShort] = useState(false)
+    const [long, setLong] = useState(false)
+    const [settings, setSettings] = useState(false)
+
+    const pomoOn = () => {
+        setShort(false)
+        setPomo(true)
+        setLong(false)
+    }
+
+    const shortOn = () => {
+        setShort(true)
+        setPomo(false)
+        setLong(false)
+    }
+    
+    const longOn = () => {
+        setLong(true)
+        setShort(false)
+        setPomo(false)
+    }
+
+    const settingsOn = () => {
+        setSettings(!settings)
+    }
 
     // Timer Display Hide Function
-        return (
-            <div class="Timer">
-                <div class="topLayer">
-                    <p>Timer</p>
-                    <p id="minIcon"><BsDash size={25}/></p>
-                </div>
-                <div class="horizontal">
-                    <div>
-                        <Pomo />
-                    </div>
-                    <div class="botLayer">
-                        <p>Pomodoro</p>
-                        <p>Short Break</p>
-                        <p>Long Break</p>
-                        <p><BsGearFill /></p>
-                    </div>
-                </div>
+    return (
+        <div class="Timer">
+            <div class="topLayer">
+                <p>Timer</p>
+                <p id="minIcon"><BsDash size={25} /></p>
             </div>
-        );
+            <div class="horizontal">
+                <div>
+                    {pomo ? <Pomo/> : null}
+                    {short ? <ShortBreak /> : null}
+                    {long ? <LongBreak /> : null}
+                </div>
+                <div class="botLayer">
+                    <p onClick={pomoOn}>Pomodoro</p>
+                    <p onClick={shortOn}>Short Break</p>
+                    <p onClick={longOn}>Long Break</p>
+                    <p onClick={settingsOn}><BsGearFill /></p>
+                </div>
+                {settings ?                 
+                <div class="timerSettings">
+                    <div class="settingsCard">
+                        <p>Pomo</p>
+                        <input type="number"></input>
+                    </div>
+                    <div class="settingsCard">
+                        <p>Short Break</p>
+                        <input type="number"></input>
+                    </div>
+                    <div class="settingsCard">
+                        <p>Long Break</p>
+                        <input type="number"></input>
+                    </div>
+                </div>
+                : null }
+
+            </div>
+        </div>
+    );
 }
 
 export default Timer;
